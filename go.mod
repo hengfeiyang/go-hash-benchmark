98014661a76f463340074470670f1500c73457ac
hengfeiyang <hengfei.yang@gmail.com>
2022-07-22 09:19:53 +0800

    add hash

--- go.mod
+++ go.mod
@@ -3,12 +3,30 @@ module github.com/hengfeiyang/go-hash-benchmark
 go 1.18
 
 require (
+	github.com/DataDog/mmh3 v0.0.0-20210722141835-012dc69a9e49
 	github.com/OneOfOne/xxhash v1.2.8
 	github.com/cespare/xxhash/v2 v2.1.2
 	github.com/creachadair/cityhash v0.1.1
+	github.com/dgryski/go-metro v0.0.0-20211217172704-adc40b04c140
+	github.com/klauspost/crc32 v1.2.0
+	github.com/minio/highwayhash v1.0.2
+	github.com/mmcloughlin/meow v0.0.0-20200201185800-3501c7c05d21
+	github.com/orisano/wyhash v1.1.0
+	github.com/shivakar/metrohash v0.0.0-20160821164112-8d1c8b6bed28
+	github.com/skeeto/pengyhash v0.0.0-20201011235239-21212c6e58a7
+	github.com/spaolacci/murmur3 v1.1.0
 	github.com/tenfyzhong/cityhash v0.0.0-20181130044406-4c2731b5918c
 	github.com/zeebo/xxh3 v1.0.2
 	github.com/zentures/cityhash v0.0.0-20131128155616-cdd6a94144ab
+	github.com/zhangyunhao116/wyhash v0.4.0
 )
 
-require github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+require (
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/shivakar/xxhash v0.0.0-20160821164220-5ea66fb45566 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
+	github.com/zhangyunhao116/sbconv v0.2.1 // indirect
+	golang.org/x/sys v0.0.0-20190130150945-aca44879d564 // indirect
+)
+
+replace github.com/zhangyunhao116/wyhash => github.com/zhangyunhao116/wyhash v0.0.0-20220217162229-7d42996fa899
--- hash_test.go
+++ hash_test.go
@@ -1,16 +1,30 @@
 package gohashbenchmark
 
 import (
+	"encoding/hex"
+	"hash/crc32"
+	"hash/crc64"
 	"hash/fnv"
 	"testing"
 
+	"github.com/DataDog/mmh3"
 	onexxhash "github.com/OneOfOne/xxhash"
 	"github.com/cespare/xxhash/v2"
 	crcityhash "github.com/creachadair/cityhash"
+	"github.com/dgryski/go-metro"
+	"github.com/hengfeiyang/go-hash-benchmark/farmhash64"
 	"github.com/hengfeiyang/go-hash-benchmark/fnv64"
+	kcrc32 "github.com/klauspost/crc32"
+	"github.com/minio/highwayhash"
+	"github.com/mmcloughlin/meow"
+	"github.com/orisano/wyhash"
+	"github.com/shivakar/metrohash"
+	"github.com/skeeto/pengyhash"
+	"github.com/spaolacci/murmur3"
 	tcityhash "github.com/tenfyzhong/cityhash"
 	"github.com/zeebo/xxh3"
 	"github.com/zentures/cityhash"
+	zwyhash "github.com/zhangyunhao116/wyhash"
 )
 
 var key = "1QvEL0YywgM"
@@ -34,6 +48,27 @@ func BenchmarkHash_fnv32_std(b *testing.B) {
 		h := fnv.New32a()
 		h.Write([]byte(key))
 		h.Sum32()
+	}
+}
+
+func BenchmarkHash_crc32(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		table := crc32.MakeTable(crc32.IEEE)
+		crc32.Checksum([]byte(key), table)
+	}
+}
+
+func BenchmarkHash_crc32_klauspost(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		table := kcrc32.MakeTable(kcrc32.IEEE)
+		kcrc32.Checksum([]byte(key), table)
+	}
+}
+
+func BenchmarkHash_crc64(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		table := crc64.MakeTable(crc64.ISO)
+		crc64.Checksum([]byte(key), table)
 	}
 }
 
@@ -74,5 +109,95 @@ func BenchmarkHash_cityhash_tenfyzhong(b *testing.B) {
 func BenchmarkHash_cityhash_creachadair(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		crcityhash.Hash64([]byte(key))
+	}
+}
+
+func BenchmarkHash_murmur3_32(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		h := murmur3.New32()
+		h.Write([]byte(key))
+		h.Sum32()
+	}
+}
+
+func BenchmarkHash_murmur3_64(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		h := murmur3.New64()
+		h.Write([]byte(key))
+		h.Sum64()
+	}
+}
+
+func BenchmarkHash_farmhash64(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		farmhash64.FarmHash64([]byte(key))
+	}
+}
+
+func BenchmarkHash_mmh3(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		mmh3.Hash32([]byte(key))
+	}
+}
+
+func BenchmarkHash_metrohash(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		h := metrohash.NewMetroHash64()
+		h.Write([]byte(key))
+		h.Sum64()
+	}
+}
+
+func BenchmarkHash_metro(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		metro.Hash64Str(key, 0)
+	}
+}
+
+func BenchmarkHash_meow_mmcloughlin(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		h := meow.New64(0)
+		h.Write([]byte(key))
+		h.Sum64()
+	}
+}
+
+func BenchmarkHash_wyhash_orisano(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		wyhash.Sum64(0, []byte(key))
+	}
+}
+
+func BenchmarkHash_wyhash_zhangyunhao116_v1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		zwyhash.Sum64String(key)
+	}
+}
+
+func BenchmarkHash_wyhash_zhangyunhao116_v3(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		zwyhash.Sum64StringV3(key)
+	}
+}
+
+func BenchmarkHash_highwayhash_minIO(b *testing.B) {
+	keyHash, _ := hex.DecodeString("000102030405060708090A0B0C0D0E0FF0E0D0C0B0A090807060504030201000") // use your own key here
+
+	for i := 0; i < b.N; i++ {
+		highwayhash.Sum64([]byte(key), keyHash)
+	}
+}
+
+func BenchmarkHash_pengyhash(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		h := pengyhash.New(1)
+		h.Write([]byte(key))
+		h.Sum(nil)
+	}
+}
+
+func BenchmarkHash_pengyhash2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		pengyhash.Pengyhash([]byte(key), uint32(len(key)))
 	}
 }
